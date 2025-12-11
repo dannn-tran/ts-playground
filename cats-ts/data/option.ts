bae993b30d9c385846644c6c327b7c654e180a81
picas9dan <picasdan99@gmail.com>
2025-12-11 21:27:14 +0800

    Refactor Option

--- cats-ts/data/option.ts
+++ cats-ts/data/option.ts
@@ -4,17 +4,20 @@ type SomeStruct<A> = {
   type: 'some'
   value: A
 }
-const NoneStruct = {
+const NONE_STRUCT = {
   type: 'none' as const
 }
+type NoneStruct = typeof NONE_STRUCT
 
-interface OptionOps<A> {
+interface OptionOpsBase<A> {
+  fold<B>(ifEmpty: () => B, f: (a: A) => B): B
+}
+type OptionOps<A> = OptionOpsBase<A> & {
   contains(elem: A): boolean
   exists(p: (a: A) => boolean): boolean
   filter(p: (a: A) => boolean): Option<A>
   filterNot(p: (a: A) => boolean): Option<A>
   flatMap<B>(f: (a: A) => Option<B>): Option<B>
-  fold<B>(ifEmpty: () => B, f: (a: A) => B): B
   forall(p: (a: A) => boolean): boolean
   foreach(f: (a: A) => void): void
   get(): A
@@ -26,7 +29,7 @@ interface OptionOps<A> {
 }
 
 type Some<A> = SomeStruct<A> & OptionOps<A>
-type None<A> = typeof NoneStruct & OptionOps<A>
+type None<A> = typeof NONE_STRUCT & OptionOps<A>
 export type Option<A> = Some<A> | None<A>
 
 export function option<A>(a: A): Option<A> {
@@ -36,42 +39,45 @@ export function option<A>(a: A): Option<A> {
 }
 
 export function some<A>(value: A): Option<A> {
-  return {
+  return augment({
     type: 'some',
     value,
-    contains: elem => value === elem,
-    exists: p => p(value),
-    filter: p => p(value) ? some(value) : none(),
-    filterNot: p => p(value) ? none() : some(value),
-    flatMap: f => f(value),
-    fold: (_, f) => f(value),
-    forall: p => p(value),
-    foreach: f => f(value),
-    get: () => value,
-    getOrElse: _ => value,
-    isDefined: () => true,
-    isEmpty: () => false,
-    map: f => some(f(value)),
-    orElse: _ => some(value)
-  }
+    fold: (_, f) => f(value)
+  })
 }
 
 export function none<A>(): Option<A> {
-  return {
+  return augment({
     type: 'none',
-    contains: _ => false,
-    exists: _ => false,
-    filter: _ => none(),
-    filterNot: _ => none(),
-    flatMap: _ => none(),
-    fold: (ifEmpty, _) => ifEmpty(),
-    forall: _ => false,
-    foreach: _ => {},
-    get: () => { throw new NoSuchElementError() },
-    getOrElse: d => d(),
-    isDefined: () => false,
-    isEmpty: () => true,
-    map: _ => none(),
-    orElse: alt => alt()
+    fold: (ifEmpty, _) => ifEmpty()
+  })
+}
+
+
+function augment<A>(base: (SomeStruct<A> | NoneStruct) & OptionOpsBase<A>): Option<A> {
+  function exists(p: (a: A) => boolean): boolean {
+    return base.fold(() => false, p)
+  }
+  function filter(p: (a: A) => boolean): Option<A> {
+    return base.fold(() => none(), a => p(a) ? some(a) : none())
+  }
+  function isDefined() {
+    return base.fold(() => false, _ => true)
+  }
+  return {
+    ...base,
+    contains: elem => base.fold(() => false, a => a === elem),
+    exists,
+    filter,
+    filterNot: p => filter(a => !p(a)),
+    flatMap: f => base.fold(() => none(), f),
+    forall: exists,
+    foreach: f => base.fold(() => {}, f),
+    get: () => base.fold(() => { throw new NoSuchElementError() }, a => a),
+    getOrElse: defaultValue => base.fold(defaultValue, a => a),
+    isDefined,
+    isEmpty: () => !isDefined(),
+    map: f => base.fold(() => none(), a => some(f(a))),
+    orElse: alt => base.fold(alt, some)
   }
 }
